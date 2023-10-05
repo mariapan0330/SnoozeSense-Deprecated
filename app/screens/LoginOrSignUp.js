1154decd2fd7ac08eaa22b577dd57ec6278cc3cd
Maria Panagos <maria.pan0330@gmail.com>
2023-10-04 23:59:43 -0500

    + signup/login screens w subsequent placeholders

--- app/screens/LoginOrSignUp.js
+++ app/screens/LoginOrSignUp.js
@@ -1,105 +1,33 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  ActivityIndicator,
-  Button,
-  KeyboardAvoidingView,
-} from "react-native";
+import { View, Text, StyleSheet, Button, Pressable } from "react-native";
 import React, { useState } from "react";
-import { FIREBASE_AUTH } from "../../FirebaseConfig";
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
+import SignUp from "./SignUp";
+import Login from "./Login";
 
-const LoginOrSignUp = () => {
+const LoginOrSignUp = ({ navigation }) => {
   /*
     LOGIN SCREEN: allows the user to enter their username and password and login.
     Currently this is also the place where they CREATE an account with the credentials they entered.
   */
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [retypePassword, setRetypePassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [isCreatingAccount, setIsCreatingAccount] = useState(false);
-  const auth = FIREBASE_AUTH;
-
-  const signIn = async () => {
-    setLoading(true);
-    try {
-      const res = await signInWithEmailAndPassword(auth, email, password);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-      if (err.message === "Firebase: Error (auth/invalid-login-credentials).") {
-        alert(
-          'Invalid Login Credentials. Click "Create Account" if you are new!'
-        );
-      } else {
-        alert("Sign In failed!" + err);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const signUp = async () => {
-    setLoading(true);
-    try {
-      const res = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-      alert("Sign Up failed " + err.message);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   return (
     <View style={styles.container}>
-      <Text>Login!!</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        autoCapitalize="none"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        autoCapitalize="none"
-        value={password}
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-      />
-      {isCreatingAccount && (
-        <TextInput
-          style={styles.input}
-          placeholder="Retype Password"
-          autoCapitalize="none"
-          value={retypePassword}
-          secureTextEntry={true}
-          onChangeText={(text) => setRetypePassword(text)}
-        />
-      )}
-
-      {loading ? (
-        <ActivityIndicator size="large" color="red" />
-      ) : (
-        <>
-          <Button title="Login" onPress={signIn} />
-          <Button title="Sign Up" onPress={signUp} />
-        </>
-      )}
+      <View style={styles.buttonContainer}>
+        <Pressable onPress={() => navigation.navigate("Login")}>
+          <View title="Login" style={[styles.button, styles.loginButton]}>
+            <Text style={{ color: "black" }}>Login</Text>
+          </View>
+        </Pressable>
+        <Pressable onPress={() => navigation.navigate("SignUp")}>
+          <View title="Sign Up" style={[styles.button, styles.signUpButton]}>
+            <Text style={{ color: "white" }}>Create Account</Text>
+          </View>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
-export default Login;
+export default LoginOrSignUp;
 
 const styles = StyleSheet.create({
   container: {
@@ -107,13 +35,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  input: {
-    marginVertical: 4,
+  buttonContainer: {
     width: "80%",
-    height: 50,
-    borderWidth: 1,
-    borderRadius: 4,
+  },
+  button: {
+    alignItems: "center",
     padding: 10,
-    backgroundColor: "lightgray",
+    paddingVertical: 10,
+    margin: 5,
+    borderRadius: 30,
+    borderWidth: 2,
+  },
+  loginButton: {
+    backgroundColor: "white",
+  },
+  signUpButton: {
+    backgroundColor: "black",
   },
 });
---
+++ app/screens/PlaceholderOnboarding.js
@@ -0,0 +1,20 @@
+import { View, Tex, StyleSheet } from "react-native";
+import React from "react";
+
+const PlaceholderOnboarding = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Placeholder Onboarding</Text>
+    </View>
+  );
+};
+
+export default PlaceholderOnboarding;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
