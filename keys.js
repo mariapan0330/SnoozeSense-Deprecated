3e11b8fb504a50302547d8e0229efb40fa6d8c77
Maria Panagos <maria.pan0330@gmail.com>
2023-10-08 03:05:03 -0500

    updated .gitignore

--- keys.js
+++
@@ -1,8 +0,0 @@
-export const KEYS = {
-  APIKEY: "AIzaSyCjz476Iu-LUly7xC7tOwrpvA8jrZFz24U",
-  AUTHDOMAIN: "snoozesense-681af.firebaseapp.com",
-  PROJECTID: "snoozesense-681af",
-  STORAGEBUCKET: "snoozesense-681af.appspot.com",
-  MESSAGESENDERID: "381149447959",
-  APPID: "1:381149447959:web:513df9b6bbc9a885051952",
-};
