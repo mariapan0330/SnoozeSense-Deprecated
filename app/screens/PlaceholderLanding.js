0edea2269c200329c9eb95c91786bb61a85f7d48
Maria Panagos <maria.pan0330@gmail.com>
2023-10-08 01:27:12 -0500

    created useUserData hook

--- app/screens/PlaceholderLanding.js
+++ app/screens/PlaceholderLanding.js
@@ -1,32 +1,33 @@
 import { View, Text, StyleSheet, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 import { FIREBASE_AUTH } from "../../services/FirebaseConfig";
-import { getUserData, updateUserData } from "../../services/handleFirestore";
+// import { getUserData, updateUserData } from "../../services/handleFirestore";
+import useUserData from "../hooks/useUserData";
 
 const PlaceholderLanding = ({ navigation }) => {
-  const [userData, setUserData] = useState({});
   const TEST_USER = "user6@gmail.com";
-
-  useEffect(() => {
-    (async () => {
-      let data = await getUserData(TEST_USER);
-      setUserData(data);
-    })();
-  }, []);
+  const { userData } = useUserData("user6@gmail.com");
 
   return (
     <View style={styles.container}>
       <Text>Welcome, {userData.username}!</Text>
       <Text>Sleep Streak: {userData.sleepStreak} days</Text>
       <Text>Sleep Duration Goal: {userData.sleepDurationGoal} hours</Text>
+      <Text>
+        Tasks:
+        {userData.tasks &&
+          userData.tasks.map((t) => (
+            <Text key={t.taskTitle}>
+              {t.taskTitle}
+              {"\n"}
+            </Text>
+          ))}
+      </Text>
       <Button
-        title="Increase sleep sleep goal hours"
-        onPress={() =>
-          // technically works (as in, it does update the db), but doesn't update the locally recognized sleep duration, so you can only do it once per load.
-          updateUserData(TEST_USER, {
-            sleepDurationGoal: userData.sleepDurationGoal + 1,
-          })
-        }
+        title="test button"
+        onPress={() => {
+          console.log(userData.tasks);
+        }}
       />
       <Button title="Log Out" />
     </View>
