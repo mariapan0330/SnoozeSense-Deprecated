dbc29cb45affba251f7214b811e7b23dd3d2b010
Maria Panagos <maria.pan0330@gmail.com>
2023-10-16 19:07:41 -0500

    removed forheads

--- app/navigation/tabs.tsx
+++ app/navigation/tabs.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ImageSourcePropType } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/Home";
@@ -14,42 +14,41 @@ const Tabs = ({ currentUser }) => {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
-        tabBarStyle: {backgroundColor: '#121524'},
-        tabBarIconStyle:{color: '#C6C6C6'},
+        tabBarStyle: { backgroundColor: "#121524" },
+        tabBarIconStyle: { color: "#C6C6C6" },
         tabBarIcon: ({ focused }) => {
-          let iconName;
+          let iconName: ImageSourcePropType;
 
-          if (route.name === 'Home') {
-            iconName = require('../images/home.png');
-          } else if (route.name === 'Calendar') {
-            iconName = require('../images/calendar.png');
-          } else if (route.name === 'Challenges') {
-            iconName = require('../images/challenges.png');
-          } else if (route.name === 'Account') {
-            iconName = require('../images/account.png');
+          if (route.name === "Home") {
+            iconName = require("../images/home.png");
+          } else if (route.name === "Calendar") {
+            iconName = require("../images/calendar.png");
+          } else if (route.name === "Challenges") {
+            iconName = require("../images/challenges.png");
+          } else if (route.name === "Account") {
+            iconName = require("../images/account.png");
           }
-          const tintColor = focused ? '#9174D0': '#C6C6C6'
-          return <Image source={iconName} style={{ width: 24, height: 24, tintColor: tintColor }} />;
+          const tintColor = focused ? "#9174D0" : "#C6C6C6";
+          return (
+            <Image
+              source={iconName}
+              style={{ width: 24, height: 24, tintColor: tintColor }}
+            />
+          );
         },
       })}
     >
-      <Tab.Screen name="Home">
+      <Tab.Screen name="Home" options={{ headerShown: false }}>
         {(props) => <HomeScreen {...props} {...{ currentUser: currentUser }} />}
       </Tab.Screen>
-      <Tab.Screen name="Calendar">
-        {(props) => (
-          <CalendarScreen {...props} {...{ currentUser: currentUser }} />
-        )}
+      <Tab.Screen name="Calendar" options={{ headerShown: false }}>
+        {(props) => <CalendarScreen {...props} {...{ currentUser: currentUser }} />}
       </Tab.Screen>
-      <Tab.Screen name="Challenges">
-        {(props) => (
-          <ChallengesScreen {...props} {...{ currentUser: currentUser }} />
-        )}
+      <Tab.Screen name="Challenges" options={{ headerShown: false }}>
+        {(props) => <ChallengesScreen {...props} {...{ currentUser: currentUser }} />}
       </Tab.Screen>
-      <Tab.Screen name="Account">
-        {(props) => (
-          <AccountScreen {...props} {...{ currentUser: currentUser }} />
-        )}
+      <Tab.Screen name="Account" options={{ headerShown: false }}>
+        {(props) => <AccountScreen {...props} {...{ currentUser: currentUser }} />}
       </Tab.Screen>
     </Tab.Navigator>
   );
